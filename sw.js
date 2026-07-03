1906b2bb09beb6017cb35797ed2cf78eb740723b
Sunil <skb060968@gmail.com>
2026-07-04 00:14:47 +0530

    v1.1.4: iPad - remove left padding, fix background color consistency

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.1.3'; // Update this version when deploying changes
+const CACHE_VERSION = '1.1.4'; // Update this version when deploying changes
 const CACHE_NAME = `bollywood-beats-v${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
