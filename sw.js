61c13a318b4643d5447cf652bde776cae2d90587
Sunil <skb060968@gmail.com>
2026-07-04 09:00:14 +0530

    v1.9.3: Increase spacing in controls block to match height of other blocks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.9.2'; // Update this version when deploying changes
+const CACHE_VERSION = '1.9.3'; // Update this version when deploying changes
 const CACHE_NAME = `bollywood-beats-v${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
