908c8a85944341497e703030d64b9cc34497e20a
Sunil <skb060968@gmail.com>
2026-07-04 10:08:38 +0530

    v1.9.5 - Revert font size, match control block height with symmetric positioning

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.9.4'; // Update this version when deploying changes
+const CACHE_VERSION = '1.9.5'; // Update this version when deploying changes
 const CACHE_NAME = `bollywood-beats-v${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
