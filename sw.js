d8afa913ab6e2998e79b170420e2ff62e2256508
Sunil <skb060968@gmail.com>
2026-07-04 08:34:20 +0530

    v1.8.0: RESET - Set keyboard keys to fixed 45x45px for ALL devices, remove all media query overrides

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.7.2'; // Update this version when deploying changes
+const CACHE_VERSION = '1.8.0'; // Update this version when deploying changes
 const CACHE_NAME = `bollywood-beats-v${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
