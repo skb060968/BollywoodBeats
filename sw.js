307cc730de25a111ff0a60f3924f5233a976e1a4
Sunil <skb060968@gmail.com>
2026-07-04 11:08:27 +0530

    v1.10.6 - Actually remove grey background boxes by deleting duplicate CSS rules

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.10.5'; // Update this version when deploying changes
+const CACHE_VERSION = '1.10.6'; // Update this version when deploying changes
 const CACHE_NAME = `bollywood-beats-v${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
