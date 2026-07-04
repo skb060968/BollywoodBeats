248774d310f207d19bcd0d1fd763566bee827aa9
Sunil <skb060968@gmail.com>
2026-07-04 08:06:31 +0530

    v1.6.3: Increase keyboard key size - bigger square keys for better usability

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.6.2'; // Update this version when deploying changes
+const CACHE_VERSION = '1.6.3'; // Update this version when deploying changes
 const CACHE_NAME = `bollywood-beats-v${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
